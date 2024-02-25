ad6cd1e3afeda0dd86325554c538d397894913cc
Muhammed Gouda <m.gouda@ptway.net>
2024-02-25 17:14:55 +0200

    add some lines

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+
 
 async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(ConfigModule.forRoot());
+
   await app.listen(5000);
 }
 bootstrap();
