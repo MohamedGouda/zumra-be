6f030e04e98b0d787a49d22b451ed0976d02e9f2
Muhammed Gouda <m.gouda@ptway.net>
2024-02-26 22:11:24 +0200

    remove un-needed line of code

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigModule } from '@nestjs/config';
-
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(ConfigModule.forRoot());
-
   await app.listen(5000);
 }
 bootstrap();
